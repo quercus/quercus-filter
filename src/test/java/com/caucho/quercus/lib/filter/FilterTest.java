782f700c1fa417cbd6dfcd979c137a65a609195c
Dominik Dorn <dominik.dorn@gmail.com>
2011-01-27 16:58:28 +0100

    initial setup for testing

---
+++ src/test/java/com/caucho/quercus/lib/filter/FilterTest.java
@@ -0,0 +1,9 @@
+package com.caucho.quercus.lib.filter;
+
+import com.caucho.quercus.junit.QuercusRunner;
+import org.junit.runner.RunWith;
+
+
+@RunWith(QuercusRunner.class)
+public class FilterTest {
+}
