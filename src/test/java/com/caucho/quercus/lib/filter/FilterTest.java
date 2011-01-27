43f5b69d46937d6688e7fbb3d995dcf8a2d331bb
Dominik Dorn <dominik.dorn@gmail.com>
2011-01-27 17:41:05 +0100

    added license header

--- src/test/java/com/caucho/quercus/lib/filter/FilterTest.java
+++ src/test/java/com/caucho/quercus/lib/filter/FilterTest.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 1998-2011 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Quercus(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Quercus Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Quercus Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Quercus Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Dominik Dorn ( http://twitter.com/domdorn )
+ */
+
 package com.caucho.quercus.lib.filter;
 
 import com.caucho.quercus.junit.QuercusRunner;
